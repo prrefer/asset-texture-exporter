a8e0422f26fc198cea9df30154b57b600afb9ca7
prefer <rjaylnprefer20081337@gmail.com>
2024-07-04 16:52:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const textureRbxmxRegex = /(?<!"MeshId">)<url>http:\/\/www\.roblox\.com\/asset\/\?id=(\d+)/
+const textureRbmxRegex = /TextureId.+\/(?:\?id=)?(\d+)/
+
 function downloadTexture(textureId) {
-	fetch(`https://thumbnails.roblox.com/v1/assets?assetIds=${textureId}&returnPolicy=PlaceHolder&size=420x420&format=Png&isCircular=false`).then(response => response.json()).then(response => {
+	fetch(`https://thumbnails.roblox.com/v1/assets?assetIds=${textureId}&returnPolicy=PlaceHolder&size=700x700&format=Png&isCircular=false`).then(response => response.json()).then(response => {
 		const textureUrl = response.data[0].imageUrl
 		chrome.downloads.download({
 			url: textureUrl,
@@ -16,10 +19,14 @@ chrome.action.onClicked.addListener(tab => {
 
 	fetch(`https://assetdelivery.roblox.com/v1/asset/?id=${assetId}`).then(response => {
 		fetch(response.url).then(response => response.text()).then(response => {
-			let textureData = response.match(/http:\/\/www.roblox.com\/asset\/\?id=([0-9]*)/)
-			if (!textureData) return
+			let textureData = response.match(textureRbxmxRegex)
+			if (!textureData) {
+				textureData = response.match(textureRbmxRegex) // Seperated into different regex checks because the OR (|) operator in the regex would not work for some reason
+				if (!textureData) return
+			}
 
 			let textureId = textureData[1]
+			console.log(textureId)
 			downloadTexture(textureId)
 		})
 	})
